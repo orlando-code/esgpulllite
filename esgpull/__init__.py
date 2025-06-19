91d0bb4565991a1dcc0422dc4f82c3d596061262
Orlando Timmerman <rt582@cam.ac.uk>
2025-06-19 10:20:02 +0100

    changed name of script from esgpull to esgpuller since causing issues with module structure

--- esgpull/__init__.py
+++ esgpull/__init__.py
@@ -1,5 +1,5 @@
 from esgpull.context import Context
-from esgpull.esgpull import Esgpull
+from esgpull.esgpuller import Esgpull
 from esgpull.models import File, Query
 from esgpull.version import __version__
 from esgpull.api import EsgpullAPI
